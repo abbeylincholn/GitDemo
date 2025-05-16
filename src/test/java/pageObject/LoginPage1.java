9fdfb7a221d24a60269561ce38985786aa26a93b
abbey <abbeyezekiel100@gmail.com>
2025-05-16 08:00:21 +0100

    this is commit from Asian Timezone guy

---
+++ src/test/java/pageObject/LoginPage1.java
@@ -0,0 +1,53 @@
+package pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage1 {
+
+	public WebDriver ldriver;
+
+	public LoginPage1(WebDriver rdriver) {
+
+		ldriver = rdriver;
+		PageFactory.initElements(rdriver, this);
+	}
+
+	@FindBy(name = "username")
+	@CacheLookup
+	WebElement Username;
+
+	@FindBy(name = "password")
+	@CacheLookup
+	WebElement Password;
+
+	@FindBy(xpath = "//button[text()=' Login ']")
+	@CacheLookup
+	WebElement Login;
+
+	@FindBy(xpath = "//a[text()='Logout']")
+	@CacheLookup
+	WebElement Logout;
+
+	public void setUserName(String uname) {
+		Username.clear();
+		Username.sendKeys(uname);
+	}
+
+	public void setPassword(String pwd) {
+		Password.clear();
+		Password.sendKeys(pwd);
+	}
+
+	public void clickLogin() {
+		Login.click();
+	}
+
+	public void clickLogout() {
+		Logout.click();
+	}
+
+}
